2a0f6edb11714733b37ec1726972ed23076526e0
Nikita Jefymov <nikita.jefymov@trivident.com>
2021-01-29 09:35:11 +0100

    Updated button styling

--- src/components/tableOfContent.js
+++ src/components/tableOfContent.js
@@ -11,16 +11,16 @@ function TableOfContent(props) {
                   style={{ margin: "20px"}}
                 >
                   <button onClick={() => props.onSelect(props.recordings[i])}>
-                    Шлоки {props.recordings[i].start} -- {props.recordings[i].end}
+                    Шлоки {props.recordings[i].start} — {props.recordings[i].end}
                   </button>
                  {i+1 < props.recordings.length ? <button onClick={() => props.onSelect(props.recordings[i+1])}>
-                    Шлоки {props.recordings[i+1].start} -- {props.recordings[i+1].end}
+                    Шлоки {props.recordings[i+1].start} — {props.recordings[i+1].end}
                   </button>: ""}
                   {i+2 < props.recordings.length ? <button onClick={() => props.onSelect(props.recordings[i+2])}>
-                    Шлоки {props.recordings[i+2].start} -- {props.recordings[i+2].end}
+                    Шлоки {props.recordings[i+2].start} — {props.recordings[i+2].end}
                   </button>: ""}
                   {i+3 < props.recordings.length ? <button onClick={() => props.onSelect(props.recordings[i+3])}>
-                    Шлоки {props.recordings[i+3].start} -- {props.recordings[i+3].end}
+                    Шлоки {props.recordings[i+3].start} — {props.recordings[i+3].end}
                   </button>: ""}
                 </div>     
                 : ""     
