263c02c8aea6817899a66b9e8787508e7af8f032
Nikita Jefymov <nikita.jefymov@trivident.com>
2021-03-18 21:07:53 +0100

    Added the latest recording

--- src/data/index.js
+++ src/data/index.js
@@ -133,6 +133,7 @@ const recordings = [
     { start: "12.15", end: "12.16", },
     { start: "12.17", end: "12.19", },
     { start: "12.19", end: "12.19", filename: "12.19.2.mp3"},
+    { start: "12.20", end: "12.20", },
 ];
 
 const data = {recordings: recordings}
